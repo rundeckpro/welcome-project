f80904d8e37598b6ca0e5eb79014b6ae8f1a1a12
Forrest Evans <fdevans@gmail.com>
2021-08-18 15:20:26 -0700

    Cloud Demo Base Commit

--- client/scripts/init.sh
+++ client/scripts/init.sh
@@ -1,7 +1,30 @@
 echo "Init Script Starting"
 ## Place Init Script Code here as needed.  This is run as part of DockerFile steps for Client node.
 
-# load project
+##Replace values from .env file on import.yml once it's on the docker image.
+sed -i 's#AWS_ACCESS_KEY#'"$AWS_ACCESS_KEY"'#g' $CONFIG_FILE
+sed -i 's#AWS_SECRET_KEY#'"$AWS_SECRET_KEY"'#g' $CONFIG_FILE
+
+sed -i 's#AZURE_API_KEY#'"$AZURE_API_KEY"'#g' $CONFIG_FILE
+sed -i 's#AZURE_CLIENTID#'"$AZURE_CLIENTID"'#g' $CONFIG_FILE
+sed -i 's#AZURE_SUBSCRIPTIONID#'"$AZURE_SUBSCRIPTIONID"'#g' $CONFIG_FILE
+sed -i 's#AZURE_TENANTID#'"$AZURE_TENANTID"'#g' $CONFIG_FILE
+
+
+
+sed -i 's#GCP_PROJECTID#'"$GCP_PROJECTID"'#g' $CONFIG_FILE
+sed -i 's#GCP_ZONE#'"$GCP_ZONE"'#g' $CONFIG_FILE
+sed -i 's#GCP_KEY_PATH#'"$GCP_KEY_PATH"'#g' $CONFIG_FILE
+
+sed -i 's#OCI_TENANTID#'"$OCI_TENANTID"'#g' $CONFIG_FILE
+sed -i 's#OCI_FINGERPRINT#'"$OCI_FINGERPRINT"'#g' $CONFIG_FILE
+sed -i 's#OCI_USERID#'"$OCI_USERID"'#g' $CONFIG_FILE
+sed -i 's#OCI_REGION#'"$OCI_REGION"'#g' $CONFIG_FILE
+sed -i 's#OCI_KEY_PATH#'"$OCI_KEY_PATH"'#g' $CONFIG_FILE
+
+cat $CONFIG_FILE
+
+# Load Rundeck Information (Projects, Users, etc.)
 ./bin/cli load --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
 ./bin/cli addUsers --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
 
