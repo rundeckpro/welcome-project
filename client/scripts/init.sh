b57e6b29a9d8f117d3516a46d914f9af62e740ec
Forrest Evans <fdevans@gmail.com>
2021-08-26 18:01:17 -0700

    Add Update to init.sh

--- client/scripts/init.sh
+++ client/scripts/init.sh
@@ -1,7 +1,14 @@
 echo "Init Script Starting"
 ## Place Init Script Code here as needed.  This is run as part of DockerFile steps for Client node.
 
+chmod +x bin/cli
+
+echo "Data Dir"
+ls -last /rundeck-cli/data
+echo "-----"
+
 # load project
 ./bin/cli load --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
+./bin/cli updateProject --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
 ./bin/cli addUsers --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
 ./bin/cli addSettings --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
